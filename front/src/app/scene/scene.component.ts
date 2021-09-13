0c2602e863f222855a366db9bd4c27f9865a0987
Nezar <57625949+Trizo1@users.noreply.github.com>
2021-09-13 20:43:52 +0300

    scene

--- front/src/app/scene/scene.component.ts
+++ front/src/app/scene/scene.component.ts
@@ -1,12 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-scene',
   templateUrl: './scene.component.html',
   styleUrls: ['./scene.component.scss'],
 })
-export class SceneComponent implements OnInit {
+export class SceneComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas') canvas: ElementRef;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    var renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas.nativeElement,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    var animate = function () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }
 }
--- front/src/app/scene/scene.module.ts
+++ front/src/app/scene/scene.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SceneRoutingModule } from './scene-routing.module';
 import { SceneComponent } from './scene.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SceneComponent],
-  imports: [CommonModule, SceneRoutingModule],
+  imports: [CommonModule, SceneRoutingModule, MatButtonModule],
 })
 export class SceneModule {}
