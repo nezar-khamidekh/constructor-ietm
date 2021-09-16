06c9d3026a26be536503aefdd1bfd89e10fc7e5b
Adetill56 <adetill56@gmail.com>
2021-09-16 19:08:23 +0300

    добавлено sidenav и header

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HeaderComponent, SidenavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
