f47450bc0a82c385439952de18ab90abe89a1e6f
Nezar <57625949+Trizo1@users.noreply.github.com>
2021-09-21 19:55:01 +0300

    test3

--- front/src/app/shared/components/test3/test3.component.ts
+++ front/src/app/shared/components/test3/test3.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test3',
   templateUrl: './test3.component.html',
-  styleUrls: ['./test3.component.scss']
+  styleUrls: ['./test3.component.scss'],
 })
-export class Test3Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Test3Component {
+  constructor() {}
 }
