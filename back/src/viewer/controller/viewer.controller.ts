2fd0e92d4c897efe08c0f028d0e318155f4ae338
TheToxicMoss <misha10dmitrov@mail.ru>
2022-02-03 15:00:31 +0300

    Запрос новых моделей

--- back/src/viewer/controller/viewer.controller.ts
+++ back/src/viewer/controller/viewer.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Response, StreamableFile } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Response,
+  StreamableFile,
+} from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 
@@ -12,5 +18,36 @@ export class ViewerController {
       join(process.cwd(), '/testmodels/valve_anim.gltf'),
     );
     return new StreamableFile(file);
+  }
+
+  @Get(':id')
+  getModel(
+    @Response({ passthrough: true }) res,
+    @Param('id') id: string,
+  ): StreamableFile {
+    switch (id) {
+      case 'valve':
+        const valve = createReadStream(
+          join(process.cwd(), '/testmodels/valve_anim.gltf'),
+        );
+        return new StreamableFile(valve);
+      case 'vice':
+        const vice = createReadStream(
+          join(process.cwd(), '/testmodels/vice.gltf'),
+        );
+        return new StreamableFile(vice);
+      case 'pump':
+        const pump = createReadStream(
+          join(process.cwd(), '/testmodels/pump.gltf'),
+        );
+        return new StreamableFile(pump);
+      case 'impeller':
+        const impeller = createReadStream(
+          join(process.cwd(), '/testmodels/impeller.gltf'),
+        );
+        return new StreamableFile(impeller);
+      default:
+        break;
+    }
   }
 }
