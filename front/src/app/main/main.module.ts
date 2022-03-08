da0f31929456fd960b13d7bbe9fb01f68370cbb7
Nezar <hamideh@yandex.ru>
2022-03-08 20:50:41 +0300

    Сверстал блоки активности на главной

--- front/src/app/main/main.module.ts
+++ front/src/app/main/main.module.ts
@@ -5,9 +5,10 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { MatButtonModule } from '@angular/material/button';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ActivityComponent } from './components/activity/activity.component';
 
 @NgModule({
-  declarations: [MainComponent, SidenavComponent],
+  declarations: [MainComponent, SidenavComponent, ActivityComponent],
   imports: [CommonModule, MainRoutingModule, MatButtonModule],
 })
 export class MainModule {}
