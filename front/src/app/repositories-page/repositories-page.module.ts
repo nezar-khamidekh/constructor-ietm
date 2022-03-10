6e29c51b31aca3dda554473cbbdffb28144702df
Nezar <hamideh@yandex.ru>
2022-03-10 20:40:48 +0300

    Добавил отображение репозиториев в виде карточек

--- front/src/app/repositories-page/repositories-page.module.ts
+++ front/src/app/repositories-page/repositories-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RepositoriesPageComponent } from './repositories-page.component';
 import { RepositoriesPageRoutingModule } from './repositories-page-routing.module';
+import { RepositoriesModule } from '../repositories/repositories.module';
 
 @NgModule({
   declarations: [RepositoriesPageComponent],
-  imports: [CommonModule, RepositoriesPageRoutingModule],
+  imports: [CommonModule, RepositoriesPageRoutingModule, RepositoriesModule],
 })
 export class RepositoriesPageModule {}
