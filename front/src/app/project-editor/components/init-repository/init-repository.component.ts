bf8eee08673c9cb59dd719efbad2b9a3bcc47940
Nezar Khamidekh <khamidekh@point-labs.com>
2022-04-10 18:40:28 +0300

    Добавил получение команд юзера и логику по отправке поля команды при создании нового репозитория

--- front/src/app/project-editor/components/init-repository/init-repository.component.ts
+++ front/src/app/project-editor/components/init-repository/init-repository.component.ts
@@ -10,8 +10,11 @@ import {
 } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
 import { DialogChooseImageComponent } from 'src/app/dialogs/dialog-choose-image/dialog-choose-image.component';
+import { RepositoryI } from 'src/app/shared/models/repository.interface';
 import { RepositoryType } from 'src/app/shared/models/repositoryTypeEnum';
+import { TeamI } from 'src/app/shared/models/team.interface';
 import { DataStoreService } from 'src/app/shared/services/data-store.service';
 import { LoadingService } from 'src/app/shared/services/loading.service';
 import { RepositoryService } from 'src/app/shared/services/repository.service';
@@ -31,17 +34,20 @@ export class InitRepositoryComponent implements OnInit, OnDestroy {
 
   repositoryGroup: FormGroup;
   repositoryPreview = '';
+  userTeams: TeamI[] = [];
 
   constructor(
     public dataStore: DataStoreService,
-    private fb: FormBuilder,
-    private loadingService: LoadingService,
     public dialog: MatDialog,
-    private cdr: ChangeDetectorRef,
+    private loadingService: LoadingService,
+    private fb: FormBuilder,
     private repositoryService: RepositoryService,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit(): void {
+    this.userTeams = this.route.snapshot.data.teams;
     this.repositoryGroup = this.fb.group({
       author: new FormControl(this.dataStore.getUserValue()?._id, [Validators.required]),
       team: new FormControl('', [Validators.required]),
@@ -71,6 +77,10 @@ export class InitRepositoryComponent implements OnInit, OnDestroy {
     this.subs.add(
       dialogRef.afterClosed().subscribe((result: { imageUrl: string; image: any }) => {
         if (result) {
+          this.repositoryPreview = result.image;
+          this.repositoryGroup.patchValue({
+            preview: result.imageUrl,
+          });
           this.cdr.detectChanges();
         }
       }),
@@ -79,8 +89,16 @@ export class InitRepositoryComponent implements OnInit, OnDestroy {
 
   create(step: number) {
     this.loadingService.setIsLoading(true);
+    const newRepository: RepositoryI = {
+      ...this.repositoryGroup.value,
+    };
+    if (newRepository.author !== this.dataStore.getUserValue()?._id) {
+      newRepository.team = newRepository.author;
+      newRepository.author = this.dataStore.getUserValue()!._id;
+    }
+
     this.subs.add(
-      this.repositoryService.create(this.repositoryGroup.value).subscribe((res) => {
+      this.repositoryService.create(newRepository).subscribe((res) => {
         this.changeStep.emit(step);
         this.loadingService.setIsLoading(false);
       }),
--- front/src/app/project-editor/project-editor-routing.module.ts
+++ front/src/app/project-editor/project-editor-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserTeamsResolverService } from '../team/resolvers/user-teams.service';
 import { ProjectEditorComponent } from './project-editor.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ProjectEditorComponent,
+    resolve: {
+      teams: UserTeamsResolverService,
+    },
   },
 ];
 
