b25fdcdbf31b29144f34cb5b9e756bc33de8abe1
Nezar Khamidekh <khamidekh@point-labs.com>
2022-04-26 18:04:59 +0300

    Вернул шаг

--- front/src/app/project-editor/project-editor.component.ts
+++ front/src/app/project-editor/project-editor.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-editor.component.scss'],
 })
 export class ProjectEditorComponent implements OnInit {
-  step: number = 3;
+  step: number = 1;
   repositoryId = '';
   filename = '';
 
