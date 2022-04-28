b20062b0bec3c35a619b79dadeeda34dcb50469a
Nezar Khamidekh <khamidekh@point-labs.com>
2022-04-28 15:50:52 +0300

    Доработал верстку страницы репозитория

--- front/src/app/repository/repository.module.ts
+++ front/src/app/repository/repository.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RepositoryRoutingModule } from './repository-routing.module';
 import { RepositoryComponent } from './repository.component';
 import { SceneModule } from '../scene/scene.module';
+import { ImageSanitizedModule } from '../shared/directives/image-sanitized/image-sanitized.module';
 
 @NgModule({
   declarations: [RepositoryComponent],
-  imports: [CommonModule, RepositoryRoutingModule, SceneModule],
+  imports: [CommonModule, RepositoryRoutingModule, SceneModule, ImageSanitizedModule],
 })
 export class RepositoryModule {}
