e5fc45b4e62a5a7fb54a919b9fc4be5d03c5dfca
Nezar Khamidekh <khamidekh@point-labs.com>
2022-05-10 15:59:43 +0300

    Добавил отображение фамилии, имени и команды в репозитории, добавление удаление репозитория, внес правки в редактирование команды

--- front/src/app/shared/models/repository.interface.ts
+++ front/src/app/shared/models/repository.interface.ts
@@ -2,11 +2,12 @@ import { RepositoryType } from './repositoryTypeEnum';
 import { ParticipantI } from './participant.interface';
 import { ModelI } from './model.interface';
 import { UserI } from './user.interface';
+import { TeamI } from './team.interface';
 
 export interface RepositoryI {
   _id: string;
   author: UserI;
-  team?: string;
+  team?: TeamI;
   title: string;
   type: RepositoryType;
   description: string;
