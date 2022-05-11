ffd586f79018aa0ccf9c79fc5d05e86feaabf613
Nezar Khamidekh <khamidekh@point-labs.com>
2022-05-11 16:37:19 +0300

    Ограничил формат загружаемых файлов модели на .stp и .gltf

--- front/src/app/project-editor/directives/dnd.directive.ts
+++ front/src/app/project-editor/directives/dnd.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Directive({
   selector: '[appDnd]',
@@ -13,7 +14,17 @@ export class DndDirective {
     const file = e.dataTransfer.files[0];
 
     if (file) {
-      this.fileDropped.emit(file);
+      const fileExtension = file.name.slice(file.name.lastIndexOf('.'));
+      if (fileExtension === '.stp' || fileExtension === '.gltf') this.fileDropped.emit(file);
+      else
+        this.snackBar.open('Неподдерживаемый формат файла, доступны: .stp и .gltf файлы', '', {
+          duration: 5000,
+          panelClass: 'errorSnack',
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
     }
   }
+
+  constructor(private snackBar: MatSnackBar) {}
 }
