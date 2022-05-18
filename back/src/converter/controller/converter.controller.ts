37e8376f792d6046f67b9404060d8d07e2ed5a1a
JoshKhrustalev <khrustalevjosh@gmail.com>
2022-05-18 13:30:39 +0300

    Converter minor fix

--- back/src/converter/controller/converter.controller.ts
+++ back/src/converter/controller/converter.controller.ts
@@ -2,13 +2,11 @@ import {
   Body,
   Controller,
   Post,
-  Res,
   StreamableFile,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Response } from 'express';
 import { createReadStream } from 'fs';
 import { diskStorage } from 'multer';
 import { extname, join } from 'path';
@@ -35,18 +33,17 @@ export class ConverterController {
       }),
     }),
   )
-  registerModel(
+  convertAndSendModel(
     @UploadedFile() file: Express.Multer.File,
     @Body() convertModelDto: ConvertModelDto,
-    @Res() res: Response,
   ) {
-    const outputPath = this.converterService.convertModel(
-      file.filename,
-      convertModelDto,
-    );
-    outputPath.subscribe((path) => {
-      const model = createReadStream(join(process.cwd(), path));
-      model.pipe(res);
-    });
+    return this.converterService
+      .convertModel(file.filename, convertModelDto)
+      .pipe(
+        map((path) => {
+          const model = createReadStream(join(process.cwd(), path));
+          return new StreamableFile(model);
+        }),
+      );
   }
 }
