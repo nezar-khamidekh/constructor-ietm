ba1eebadca65a85014767093eee5ee77dc61f7cf
JoshKhrustalev <khrustalevjosh@gmail.com>
2022-05-23 02:07:28 +0300

    Model-manager module base

--- back/src/model-manager/model-manager.module.ts
+++ back/src/model-manager/model-manager.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConverterController } from './controller/converter.controller';
 import { ModelController } from './controller/model.controller';
+import { ConverterService } from './service/converter.service';
 import { ModelService } from './service/model.service';
 
 @Module({
-  controllers: [ModelController],
-  providers: [ModelService],
-  exports: [ModelService],
+  controllers: [ModelController, ConverterController],
+  providers: [ModelService, ConverterService],
+  exports: [ModelService, ConverterService],
 })
 export class ModelManagerModule {}
---
+++ back/src/model-manager/service/converter.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { ConvertModelDto } from '../models/dto/convertModel.dto';
+import * as nrc from 'node-run-cmd';
+import * as fs from 'fs';
+import { ConfigService } from '@nestjs/config';
+import { join, basename, extname } from 'path';
+import { from, map } from 'rxjs';
+
+@Injectable()
+export class ConverterService {
+  DEFAULT_PATH = './buffer/';
+  CONVERTER_PATH: string;
+
+  constructor(private configService: ConfigService) {
+    this.CONVERTER_PATH = this.configService.get('CONVERTER_PATH');
+  }
+
+  convertModel(filePath: string, params: ConvertModelDto) {
+    const inputPath = this.inputPath(filePath);
+    const outputPath = this.outputPath(filePath, params.format);
+    return from(
+      isNaN(params.compression)
+        ? nrc.run([`gltf-converter ${inputPath} ${outputPath}`], {
+            cwd: this.CONVERTER_PATH,
+          })
+        : nrc.run(
+            [
+              `gltf-converter ${inputPath} ${outputPath} --draco --speed=${
+                10 - params.compression
+              }`,
+            ],
+            {
+              cwd: this.CONVERTER_PATH,
+            },
+          ),
+    ).pipe(
+      map(() => {
+        return outputPath;
+      }),
+    );
+  }
+
+  inputPath(filePath: string) {
+    return join(this.DEFAULT_PATH, filePath);
+  }
+
+  outputPath(filePath: string, format: string) {
+    return join(
+      this.DEFAULT_PATH,
+      basename(filePath, extname(filePath)) + '.' + format,
+    );
+  }
+
+  deleteModel(path: string) {
+    return from(
+      fs.promises
+        .rm(path, { force: true, recursive: true })
+        .then(() => true)
+        .catch(() => false),
+    );
+  }
+}
