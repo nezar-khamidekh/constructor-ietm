7edf63fb20443dbcf1f6a227dd4ef0178caddc38
JoshKhrustalev <khrustalevjosh@gmail.com>
2022-06-05 15:33:55 +0300

    Minor fixes for listing

--- back/src/model-manager/controller/model.controller.ts
+++ back/src/model-manager/controller/model.controller.ts
@@ -7,16 +7,18 @@ import {
 } from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
-import { ModelService } from '../service/model.service';
 
 @Controller('model')
 export class ModelController {
-  constructor(private modelService: ModelService) {}
+  // Folder with test models
+  private folder = 'testmodels';
+
+  constructor() {}
 
   @Get('default')
   defaultModel(@Response({ passthrough: true }) res): StreamableFile {
     const file = createReadStream(
-      join(process.cwd(), '/testmodels/modelDraco.gltf'),
+      join(process.cwd(), this.folder, 'defaultModel.gltf'),
     );
     return new StreamableFile(file);
   }
@@ -29,27 +31,27 @@ export class ModelController {
     switch (id) {
       case 'valve':
         const valve = createReadStream(
-          join(process.cwd(), '/testmodels/valve_anim.gltf'),
+          join(process.cwd(), this.folder, 'valve.gltf'),
         );
         return new StreamableFile(valve);
       case 'vice':
         const vice = createReadStream(
-          join(process.cwd(), '/testmodels/vice.gltf'),
+          join(process.cwd(), this.folder, 'vice.gltf'),
         );
         return new StreamableFile(vice);
       case 'pump':
         const pump = createReadStream(
-          join(process.cwd(), '/testmodels/pump.gltf'),
+          join(process.cwd(), this.folder, 'pump.gltf'),
         );
         return new StreamableFile(pump);
       case 'impeller':
         const impeller = createReadStream(
-          join(process.cwd(), '/testmodels/impeller.gltf'),
+          join(process.cwd(), this.folder, 'impeller.gltf'),
         );
         return new StreamableFile(impeller);
       default:
         const other = createReadStream(
-          join(process.cwd(), `/testmodels/${id}.gltf`),
+          join(process.cwd(), this.folder, `${id}.gltf`),
         );
         return new StreamableFile(other);
     }
