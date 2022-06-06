47c88c4e55281913efb72bfc68075732408d05e1
Zhuravlev Mikhail <zhuravlev@point-labs.com>
2022-06-07 00:00:58 +0300

    Фикс ограничения на размер запроса

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,12 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
   app.enableCors({ credentials: true, origin: true });
   await app.listen(process.env.PORT || 8080);
 }
